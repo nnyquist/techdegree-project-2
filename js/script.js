d28d31234b73caca96d0bc54798f09fd0936765a
NateTheOk <nnyquist@thinkbrg.com>
2020-03-02 14:15:05 -0500

    refactor 1

--- js/script.js
+++ js/script.js
@@ -3,32 +3,24 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+/*****************************************
+    User-defined functions
+*****************************************/
+
 // create function to handle element creation
-function creation(elementName, property, value) {
+// allows for 2 property/value combos, 1, or none.
+function creation(
+    elementName,
+    prop1 = undefined,
+    val1 = undefined,
+    prop2 = undefined,
+    val2 = undefined
+    ) {
     const element = document.createElement(elementName);
-    element[property] = value;
+    element[prop1] = val1;
+    element[prop2] = val2;
     return element;
 }
-
-// add search component
-const searchBox = creation('div', 'className', 'student-search');
-
-const searchInput = creation('input', 'type', 'text');
-searchInput.placeholder = 'Search for students ...';
-
-const searchButton = creation('button', 'textContent', 'Search');
-
-searchBox.appendChild(searchInput);
-searchBox.appendChild(searchButton);
-
-document.querySelector('.page-header').appendChild(searchBox);
-
-// Create global variables to house list items and pagination number
-const students = document.querySelector('.student-list')
-    .children;
-const paginationNum = 10;
-const divPage = document.querySelector('.page');
-
 
 // create showPage function to limit to students by paginationNum
 function showPage(list, page) {
@@ -41,7 +33,6 @@ function showPage(list, page) {
     // loop through the list and display any item that is within the index range above
     for (i = 0; i < list.length; i += 1) {
         selectedLi = list[i];
-
         if (i >= startIndex && i < endIndex) {
             selectedLi.style.display = 'list-item';
         } else {
@@ -50,13 +41,12 @@ function showPage(list, page) {
     }
 }
 
-
 // create appendPageLinks function to make links for each 10 student page
 function appendPageLinks(list) {
     // create DOM elements
     const div = creation('div', 'className', 'pagination');
 
-    const ul = document.createElement('ul');
+    const ul = creation('ul');
 
     // page limit based on number of students in list
     const pageLimit = Math.ceil(list.length/paginationNum);
@@ -64,10 +54,10 @@ function appendPageLinks(list) {
     // for every page have one li element in the ul element
     for (i = 0; i < pageLimit; i += 1){
         // create the li and a elements
-        const li = document.createElement('li');
+        const li = creation('li');
         
-        const aLink = creation('a', 'href', '#');
-        aLink.textContent = i + 1;
+        const aLink = creation('a', 'href', '#',
+            'textContent', i + 1);
 
         // add active class name to first pagination link
         if (i === 0) {
@@ -119,84 +109,93 @@ function searchNames(list, searchText){
     return matchArray;
 }
 
+/*****************************************
+    DOM Element Creation
+*****************************************/
+
+// add search component
+const searchBox = creation('div', 'className', 'student-search');
+
+const searchInput = creation('input', 'type', 'text',
+    'placeholder', 'Search for students ...');
+
+const searchButton = creation('button', 'textContent', 'Search');
+
+searchBox.appendChild(searchInput);
+searchBox.appendChild(searchButton);
+
+document.querySelector('.page-header').appendChild(searchBox);
+
+// Create global variables to house list items and pagination number
+const students = document.querySelector('.student-list')
+    .children;
+const paginationNum = 10;
+const divPage = document.querySelector('.page');
+
+
+
+/*****************************************
+    Program Initialization
+*****************************************/
+
 // initial execution of the functions above
 showPage(students,1);
 appendPageLinks(students);
 
 // create a div paragraph if no results show up
-const noResultsDiv = document.createElement('div');
-noResultsDiv.className = 'no-results';
+const noResultsDiv = creation('div', 'className', 'no-results');
 noResultsDiv.style.display = 'none' // initialize to not display
 
-const noResultsP = document.createElement('p');
-noResultsP.textContent = "I'm sorry, there is no student that matches your search result.";
+const noResultsP = creation('p', 'textContent',
+    "I'm sorry, there is no student that matches your search result.");
 
 noResultsDiv.appendChild(noResultsP);
 const ulPage = document.querySelector('.pagination');
 divPage.insertBefore(noResultsDiv, ulPage);
 
-// function to return all pagination links
-// pageLinks = document.querySelectorAll('.pagination a')
 
-// add a "click" event listener to each A element
-// for (i = 0; i < pageLinks.length; i += 1){
-//     let a = pageLinks[i];
+/*****************************************
+    Search Click Event Handlers
+*****************************************/
 
-//     a.addEventListener('click', (e) => {
-//         for (j = 0; j < pageLinks.length; j ++){
-//             pageLinks[j].className = '';
-//         }
-//         e.target.className = 'active';
-//         showPage(students, e.target.textContent)
-//     })
-// }
+function searchProcess() {
+    // hide the "no results" display
+    noResultsDiv.style.display = 'none';
+    searchText = searchInput.value;
 
+    // initialize matched as the result of the searchNames func
+    let matched = searchNames(students, searchText);
+
+    // if the searchText is empty, show everything
+    if (searchText === ''){
+        matched = students;
+    // else if the searchNames returned nothing show the "no results"
+    } else if (matched.length === 0){
+        noResultsDiv.style.display = 'block';
+    }
+
+    // drop the pagination class div in preparation of recreation below
+    divPage.removeChild(
+        document.querySelector('.pagination')
+    );
+    // hide all students to not interfere with new showPage
+    for (i = 0; i < students.length; i++){
+        students[i].style.display = 'none';
+    }
+
+    // show and paginate the results of the search
+    showPage(matched, 1);
+    appendPageLinks(matched);
+}
 
 // click event for search box if button clicked
 searchButton.addEventListener('click', (e) => {
     e.preventDefault();
-    searchText = searchInput.value;
-
-    let matched = searchNames(students, searchText);
-
-    if (searchText === ''){
-        matched = students;
-    }
-
-    // drop the pagination class div
-    divPage.removeChild(
-        document.querySelector('.pagination')
-    );
-    // hide all students
-    for (i = 0; i < students.length; i++){
-        students[i].style.display = 'none';
-    }
-    showPage(matched, 1);
-    appendPageLinks(matched);
+    searchProcess();
 });
 
 // keyup event for search box as text entered
 searchInput.addEventListener('keyup', (e) => {
     e.preventDefault();
-    noResultsDiv.style.display = 'none';
-    searchText = searchInput.value;
-
-    let matched = searchNames(students, searchText);
-
-    if (searchText === ''){
-        matched = students;
-    } else if (matched.length === 0){
-        noResultsDiv.style.display = 'block';
-    }
-
-    // drop the pagination class div
-    divPage.removeChild(
-        document.querySelector('.pagination')
-    );
-    // hide all students
-    for (i = 0; i < students.length; i++){
-        students[i].style.display = 'none';
-    }
-    showPage(matched, 1);
-    appendPageLinks(matched);
+    searchProcess();
 });
